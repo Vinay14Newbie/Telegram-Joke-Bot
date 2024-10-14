9a24b71e5613982d40de3f9a80c1607c257bb2d6
Vinay14Newbie <wjust1984@gmail.com>
2024-10-14 21:12:42 +0530

    some modification, added my and my friends name

--- index.js
+++ index.js
@@ -15,8 +15,34 @@ bot.on('message', (msg)=>{
     const text = msg.text;
     console.log("message received: ", text);
 
-    // every chat has it's unique id
-    bot.sendMessage(msg.chat.id, 'you said: '+ text)
+    if(text==="vinay"){
+        bot.sendMessage(msg.chat.id, `Hello Master, How can i help you :),
+            I'm glad that you remember me,
+            Is there anything that you need...
+        `)
+    }
+    else if(text==="mangesh"){
+        bot.sendMessage(msg.chat.id, `Hello Mangesh, I know you,
+            Your're the son of Prabhuji,
+            Your age is 22,
+            You're a male,
+            You have a shortage of height,
+            & you need to go to gym for your health, cause you look very skinny    
+        `)
+    }
+    else if(text==="kunal"){
+        bot.sendMessage(msg.chat.id, `Hello Kunal, I know you pervert,
+            always looking for a hot girl,
+            & drooling on them,
+            you still have time be kind towards them.
+            Greet from your friend Vinay :)    
+        `)
+    }
+    else{
+        // every chat has it's unique id
+        bot.sendMessage(msg.chat.id, 'you said: '+ text)
+    }
+
 })
 
 bot.onText(/\/start/, (msg)=>{
